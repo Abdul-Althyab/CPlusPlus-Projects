c7bcaf28c928326d0d6a22a3819e9c7b8c5a15ea
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-05 20:54:27 +0100

    Refactor time calculations for clarity and consistency
    Updated time calculations to use `pow` for better readability
    and consistency. Changed variable types from `unsigned short`
    to `unsigned int` to prevent overflow. Added `#include <cmath>`
    to support the `pow` function.

--- Problem2_43Seconds to Days Hours Minutes Seconds/Problem2_43Seconds to Days Hours Minutes Seconds.cpp
+++ Problem2_43Seconds to Days Hours Minutes Seconds/Problem2_43Seconds to Days Hours Minutes Seconds.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
 {
 	unsigned int totalSeconds;
-	unsigned short days, hours, minutes, seconds, remainingSeconds;
+	unsigned int days, hours, minutes, seconds,remainingSeconds;
 	cout << "This program converts a duration given in seconds into days, hours, minutes, and seconds." << endl;
 	cout << "Enter the total number of seconds: ";
 	cin >> totalSeconds;
 
-	days = floor(totalSeconds / (24 * 60 * 60));
-	remainingSeconds = totalSeconds % (24 * 60 * 60);
-	hours = floor(remainingSeconds / (60 * 60));
-	remainingSeconds = remainingSeconds % (60 * 60);
+	days = floor(totalSeconds / (24 * pow(60, 2)));
+	remainingSeconds = totalSeconds % int(24 * pow(60, 2));
+	hours = floor(remainingSeconds / pow(60, 2));
+	remainingSeconds = remainingSeconds % int(pow(60, 2));
 	minutes = floor(remainingSeconds / 60);
 	seconds = remainingSeconds % 60;
 	cout << totalSeconds << " seconds is equivalent to " << days << " days, " << hours << " hours, " << minutes << " minutes, and " << seconds << " seconds." << endl;
