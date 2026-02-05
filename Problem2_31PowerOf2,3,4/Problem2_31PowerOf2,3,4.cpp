0c5ab075ce38f752ab6950791417d7af4aa14bbb
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-05 15:10:00 +0100

    Refactor power calculations and clean up comments
    Replaced manual power calculations with `pow` function from `<cmath>`
    for improved readability and conciseness. Updated `#include` directive
    to use `<cmath>` instead of `<iostream>`. Removed outdated comments
    at the top and bottom of the file to streamline the code.

--- Problem2_31PowerOf2,3,4/Problem2_31PowerOf2,3,4.cpp
+++ Problem2_31PowerOf2,3,4/Problem2_31PowerOf2,3,4.cpp
@@ -1,7 +1,5 @@
-// Problem2_31PowerOf2,3,4.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
 using namespace std;
 
 int main()
@@ -11,18 +9,9 @@ int main()
 	cout << "Enter a number:\n";
 	cin >> number;
 	cout << endl;
-	cout << "The square of " << number << " is " << number * number << endl;
-	cout << "The cube of " << number << " is " << number * number * number << endl;
-	cout << "The fourth power of " << number << " is " << number * number * number * number << endl;
+	cout << "The square of " << number << " is " << pow(number, 2) << endl;
+	cout << "The cube of " << number << " is " << pow(number, 3) << endl;
+	cout << "The fourth power of " << number << " is " << pow(number, 4) << endl;
 	return 0;
 }
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
