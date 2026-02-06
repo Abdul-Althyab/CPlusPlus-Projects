76fa00098c7eeda4c2379dec5ddbf43e4e6edf87
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-06 10:56:55 +0100

    Add Problem2_32Power of M project to Homework4 solution
    Added a new project, Problem2_32Power of M, to the Homework4.sln
    solution file, including its configuration for Debug and Release
    modes on x86 and x64 platforms. Updated the solution's global
    configuration section to include the new project's build settings.
    
    Created the source file Problem2_32Power of M.cpp, implementing
    a program to calculate the power of a base number using <cmath>'s
    pow function with user input/output.
    
    Added the project file Problem2_32Power of M.vcxproj, defining
    build configurations and platform settings, and the filters file
    Problem2_32Power of M.vcxproj.filters for logical file grouping.

---
+++ Problem2_32Power of M/Problem2_32Power of M.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	double base, exponent, result;
+	cout << "This program calculates the power of M\n" << endl;
+	cout << "Please enter the base: ";
+	cin >> base;
+	cout << endl;
+	cout << "Please enter the exponent: ";
+	cin >> exponent;
+	cout << endl;
+	result = pow(base, exponent);
+	cout << base << " raised to the power of " << exponent << " is: " << result;
+}
