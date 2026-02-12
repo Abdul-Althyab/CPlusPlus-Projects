603d56ac1f5f1053f6ea833bfb2188ec066f9cc4
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 12:50:29 +0100

    Refactor circle area calculation logic
    Introduced a new `calculateAreaOfCircle` function to encapsulate
    the logic for calculating the area of a circle, improving
    modularity and readability. Removed the `PI` constant from
    `main` and defined it within the new function with higher
    precision. Added `#include <cmath>` to enable the use of `pow`.
    Explicitly added `return 0;` to `main` for clarity. Removed
    the outdated top-of-file comment.

--- Problem2_19CircleAreaThroughDiameter/Problem2_19CircleAreaThroughDiameter.cpp
+++ Problem2_19CircleAreaThroughDiameter/Problem2_19CircleAreaThroughDiameter.cpp
@@ -1,19 +1,22 @@
-// Problem2_19CircleAreaThroughDiameter.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cmath>
 using namespace std;
 
+float calculateAreaOfCircle(float diameter) {
+	const float PI = 3.14159265359f;
+	
+	return PI * pow((diameter / 2.0), 2);
+}
 int main()
 {
 	unsigned short diameter;
-	const float PI = 3.14;
-
 	float area;
+
 	cout << "This program calculates the area of a circle." << endl;
 	cout << "Enter the diameter of the circle:\n";
 	cin >> diameter;
 
-	area = PI * pow((diameter / 2.0), 2);
+	area = calculateAreaOfCircle(diameter);
 	cout << "The area of the circle = " << area << endl;
+	return 0;
 }
