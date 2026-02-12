0686bc643885db159c9869548bb2a943eed188ef
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 13:55:57 +0100

    Refactor: Extract circle area logic into a new function
    Introduced a new `areaOfCircle` function to encapsulate the logic
    for calculating the area of the circle described around a triangle.
    Moved the `PI` constant to this function and updated its precision.
    Simplified the `main` function by delegating the area calculation
    to `areaOfCircle`, improving readability and modularity. Added an
    explicit `return 0;` to the `main` function for clarity.

--- Problem2_23Circle Area circle Described Around an Arbitrary Triangle/Problem2_23Circle Area circle Described Around an Arbitrary Triangle.cpp
+++ Problem2_23Circle Area circle Described Around an Arbitrary Triangle/Problem2_23Circle Area circle Described Around an Arbitrary Triangle.cpp
@@ -2,10 +2,17 @@
 #include <cmath>
 using namespace std;
 
+double areaOfCircle(double sideA, double sideB, double sideC)
+{
+    const float PI = 3.14159265359f;
+	double p = (sideA + sideB + sideC) / 2.0;
+	return PI * pow((sideA * sideB * sideC) / (4 * sqrt(p * (p - sideA) * (p - sideB) * (p - sideC))), 2);
+}
+
 int main()
 {
-    double sideA, sideB, sideC, p, area;
-    const float PI = 3.14159;
+    double sideA, sideB, sideC, area;
+    
 
     cout << "This program calculates Circle Area that circle Described Around an Arbitrary Triangle\n" << endl;
     cout << "Please enter the first side of the triangle: ";
@@ -18,9 +25,7 @@ int main()
     cin >> sideC;
     cout << endl;
 
-    p = (sideA + sideB + sideC) / 2.0;
-    area = PI * pow((sideA * sideB * sideC) / (4 * sqrt(p * (p - sideA) * (p - sideB) * (p - sideC))), 2);
-
+	area = areaOfCircle(sideA, sideB, sideC);
     cout << "The circle area is:  " << area;
-
+	return 0;
 }
