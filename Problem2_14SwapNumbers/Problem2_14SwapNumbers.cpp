8e2115e7f2767fbd2c8e72b15c07f35547db1dfa
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 09:14:52 +0100

    Refactor swapping logic into SwapTwoNumbers function
    A new function `SwapTwoNumbers` was introduced to encapsulate
    the swapping logic, improving code modularity and readability.
    The `temp` variable was removed from the `main` function, as
    it is now redundant. The `#include <iostream>` and `using
    namespace std;` statements were reformatted for clarity.
    Whitespace was added at the end of `main` for better readability.

--- Problem2_14SwapNumbers/Problem2_14SwapNumbers.cpp
+++ Problem2_14SwapNumbers/Problem2_14SwapNumbers.cpp
@@ -1,10 +1,16 @@
-
-
 #include <iostream>
 using namespace std;
+
+void SwapTwoNumbers(int &num1, int &num2)
+{
+	int temp;
+	temp = num1;
+	num1 = num2;
+	num2 = temp;
+	}
 int main()
 {
-	int num1, num2, temp;
+	int num1, num2;
 
 	cout << "This program swaps the values of two numbers." << endl;
 	cout << "Enter the first number:\n";
@@ -13,10 +19,10 @@ int main()
 	cin >> num2;
 
 	cout << "Before swapping, num1 is " << num1 << " and num2 is " << num2 << endl;
-	temp = num1;
-	num1 = num2;
-	num2 = temp;
+	SwapTwoNumbers(num1, num2);
 	cout << "After swapping, num1 is " << num1 << " and num2 is " << num2 << endl;
+	
+	
 	return 0;
 }
 
