03375c0d8eec48e8561e12db80666aec3f54c719
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 09:32:47 +0100

    Refactor area calculation and improve code clarity
    Refactored the area calculation logic by introducing a new
    `CalculateArea` function, improving reusability and readability.
    Removed the `area` variable from `main` and updated the output
    statement to directly call `CalculateArea(length, width)`.
    Modified `#include <iostream>` to fix formatting issues.
    Added `return 0;` to explicitly indicate successful termination.

--- Problem2_15RectangleArea/Problem2_15RectangleArea.cpp
+++ Problem2_15RectangleArea/Problem2_15RectangleArea.cpp
@@ -1,16 +1,21 @@
-
 #include <iostream>
 using namespace std;
+
+float CalculateArea(unsigned short length, unsigned short width)
+{
+		return (float)length * (float)width;
+}
+
 int main()
 {
     unsigned short length, width;
-	float area;
+	
 	cout << "This program calculates the area of a rectangle." << endl;
 	cout << "Enter the length of the rectangle:\n";
 	cin >> length;
 	cout << "Enter the width of the rectangle:\n";
 	cin >> width;
-	area = length * width;
-	cout << "The area of the rectangle is " << area << endl;
-
-}
+	
+	cout << "The area of the rectangle is " << CalculateArea(length, width) << endl;
+	return 0;
+	}
