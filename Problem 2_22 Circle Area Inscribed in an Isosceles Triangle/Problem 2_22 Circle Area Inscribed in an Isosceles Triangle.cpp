c886d504eee533ce24fa5e807b54193014e6b749
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 13:45:34 +0100

    Refactor circle area logic and improve code clarity
    Encapsulated circle area calculation in a new `areaOfCircle`
    function for better modularity and reusability. Moved and
    updated the `Pi` constant to a more precise value within the
    new function. Simplified `main` by delegating the circle area
    calculation to `areaOfCircle`. Reformatted the introductory
    message for clarity and explicitly added `return 0;` to
    indicate successful program termination. Included `<cmath>`
    for mathematical functions like `pow`.

--- Problem 2_22 Circle Area Inscribed in an Isosceles Triangle/Problem 2_22 Circle Area Inscribed in an Isosceles Triangle.cpp
+++ Problem 2_22 Circle Area Inscribed in an Isosceles Triangle/Problem 2_22 Circle Area Inscribed in an Isosceles Triangle.cpp
@@ -1,19 +1,23 @@
-
 #include <iostream>
+#include <cmath>
 using namespace std;
+
+double areaOfCircle(double side, double base)
+{
+	const float Pi = 3.14159265359f;
+	return Pi * (pow(base, 2) / 4) * ((2 * side - base) / (2 * side + base));
+}
 int main()
 {
 	    double area, side, base;
-		const double Pi = 3.14;
-
+		
 		cout << "This program calculates the area of a circle inscribed in an isosceles triangle." << endl;
 		cout << "Enter the length of the equal sides of the isosceles triangle:\n";
 		cin >> side;
 		cout << "Enter the length of the base of the isosceles triangle:\n";
 		cin >> base;
 
-		area = Pi * (pow(base, 2) / 4) * ((2 * side - base) / (2 * side + base));
-
+		area = areaOfCircle(side, base);
 		cout << "The area of the circle inscribed in the isosceles triangle = " << area << endl;
-
+		return 0;
 }
