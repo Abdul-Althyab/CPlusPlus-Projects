1a62f4fd67398a25f317c1c4ecae46e8a072c83c
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 10:07:54 +0100

    Refactor circle area calculation logic
    Encapsulated the circle area calculation in a new function
    `calculateAreaOfCircle`, which defines a high-precision `PI`
    constant and uses it for the computation. Updated the `main`
    function to use this new function, removing the redundant `PI`
    constant. Added an explicit `return 0;` to the `main` function
    for clarity.

--- Problem2_18Circle Area/Problem2_18Circle Area.cpp
+++ Problem2_18Circle Area/Problem2_18Circle Area.cpp
@@ -2,15 +2,20 @@
 #include <cmath>
 using namespace std;
 
+float calculateAreaOfCircle(float radius) {
+	const float PI = 3.14159265359f;
+	return PI * pow(radius, 2);
+}
+
 int main()
 {
-	const float PI = 3.14;
 	float radius, area;
 
 	cout << "This program calculates the area of a circle." << endl;
 	cout << "Enter the radius of the circle: ";
 	cin >> radius;
 
-	area = PI * pow(radius, 2);
+	area = calculateAreaOfCircle(radius);
 	cout << "The area of the circle is: " << area << endl;
+	return 0;
 }
