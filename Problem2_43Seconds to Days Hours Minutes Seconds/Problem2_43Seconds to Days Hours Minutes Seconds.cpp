47ba76fba62db796f2c364b22ab57f222344368f
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 15:03:25 +0100

    Refactor main() by extracting convertSeconds() function
    Refactored the `main()` function to improve modularity by
    extracting the conversion logic into a new `convertSeconds()`
    function. This enhances code reusability and maintainability.
    The `main()` function now focuses on user input/output, while
    `convertSeconds()` handles the conversion of seconds into
    days, hours, minutes, and seconds. Removed the `totalSeconds`
    variable from `main()` and passed it as an argument to the
    new function.

--- Problem2_43Seconds to Days Hours Minutes Seconds/Problem2_43Seconds to Days Hours Minutes Seconds.cpp
+++ Problem2_43Seconds to Days Hours Minutes Seconds/Problem2_43Seconds to Days Hours Minutes Seconds.cpp
@@ -2,14 +2,9 @@
 #include <cmath>
 using namespace std;
 
-int main()
+void convertSeconds(unsigned int totalSeconds) 
 {
-	unsigned int totalSeconds;
-	unsigned int days, hours, minutes, seconds,remainingSeconds;
-	cout << "This program converts a duration given in seconds into days, hours, minutes, and seconds." << endl;
-	cout << "Enter the total number of seconds: ";
-	cin >> totalSeconds;
-
+	unsigned int days, hours, minutes, seconds, remainingSeconds;
 	days = floor(totalSeconds / (24 * pow(60, 2)));
 	remainingSeconds = totalSeconds % int(24 * pow(60, 2));
 	hours = floor(remainingSeconds / pow(60, 2));
@@ -17,5 +12,16 @@ int main()
 	minutes = floor(remainingSeconds / 60);
 	seconds = remainingSeconds % 60;
 	cout << totalSeconds << " seconds is equivalent to " << days << " days, " << hours << " hours, " << minutes << " minutes, and " << seconds << " seconds." << endl;
+}
 
+int main()
+{
+	unsigned int totalSeconds;
+
+	cout << "This program converts a duration given in seconds into days, hours, minutes, and seconds." << endl;
+	cout << "Enter the total number of seconds: ";
+	cin >> totalSeconds;
+
+	convertSeconds(totalSeconds);
+	return 0;
 }
