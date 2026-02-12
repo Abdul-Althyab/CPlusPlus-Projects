fc3c81fce6f4576067e7989f235f6b06fb3a18c4
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 14:07:44 +0100

    Refactor power calculations into a new function
    Encapsulated the logic for calculating and displaying the square,
    cube, and fourth power of a number in a new function
    `calculatePowers(double number)`. Updated the `main` function to
    use this new function, replacing inline calculations and output
    statements. Changed the `number` variable in `main` from `int`
    to `double` to support non-integer input. Removed redundant
    calculation code from `main` as it is now handled by
    `calculatePowers`.

--- Problem2_31PowerOf2,3,4/Problem2_31PowerOf2,3,4.cpp
+++ Problem2_31PowerOf2,3,4/Problem2_31PowerOf2,3,4.cpp
@@ -2,16 +2,21 @@
 #include <cmath>
 using namespace std;
 
+void calculatePowers(double number)
+{
+	cout << "The square of " << number << " is " << pow(number, 2) << endl;
+	cout << "The cube of " << number << " is " << pow(number, 3) << endl;
+	cout << "The fourth power of " << number << " is " << pow(number, 4) << endl;
+}
+
 int main()
 {
-	int number;
+	double number;
 	cout << "This program calculates the square, cube, and fourth power of a number you enter." << endl;
 	cout << "Enter a number:\n";
 	cin >> number;
 	cout << endl;
-	cout << "The square of " << number << " is " << pow(number, 2) << endl;
-	cout << "The cube of " << number << " is " << pow(number, 3) << endl;
-	cout << "The fourth power of " << number << " is " << pow(number, 4) << endl;
+	calculatePowers(number);
 	return 0;
 }
 
