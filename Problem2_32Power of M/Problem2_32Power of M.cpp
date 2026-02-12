88696218389d7ffe0ce1168c18e1de5b1b6c9e12
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-12 14:18:13 +0100

    Add `power` function and update `main` to use it
    A new `power` function was introduced to encapsulate the logic
    for calculating `base` raised to `exponent`. This improves
    code readability and reusability. The `main` function was
    updated to use `power` instead of directly calling `pow`.
    
    Additionally, a `return 0;` statement was added to the end of
    `main` to explicitly indicate successful program termination.

--- Problem2_32Power of M/Problem2_32Power of M.cpp
+++ Problem2_32Power of M/Problem2_32Power of M.cpp
@@ -2,6 +2,11 @@
 #include <cmath>
 using namespace std;
 
+double power(double base, double exponent)
+{
+	return pow(base, exponent);
+}
+
 int main()
 {
 	double base, exponent, result;
@@ -12,6 +17,7 @@ int main()
 	cout << "Please enter the exponent: ";
 	cin >> exponent;
 	cout << endl;
-	result = pow(base, exponent);
+	result = power(base, exponent);
 	cout << base << " raised to the power of " << exponent << " is: " << result;
+	return 0;
 }
