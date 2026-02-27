71a19cff4717682b6cbb7a9a9a07bdc152b3eb02
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-27 10:38:47 +0100

    Refactor main function for modularity and readability
    Refactored the `main` function by extracting the logic for
    reading user input and checking pass/fail status into two
    new functions: `readMark` and `checkPassFail`. This improves
    code organization by separating concerns, making the code
    easier to read, maintain, and test. The original inline logic
    was removed, and the `main` function now delegates these
    tasks to the new helper functions.

--- Problem2_8Pass-Fail/Problem2_8Pass-Fail.cpp
+++ Problem2_8Pass-Fail/Problem2_8Pass-Fail.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void readMark(double& mark)
 {
-	double mark;
 	cout << "Enter your mark: ";
 	cin >> mark;
-
+}
+void checkPassFail(const double mark)
+{
 	if (mark >= 50)
 	{
 		cout << "You passed!" << endl;
@@ -15,5 +16,23 @@ int main()
 	{
 		cout << "You failed." << endl;
 	}
+}
+
+int main()
+{
+	double mark;
+	//cout << "Enter your mark: ";
+	//cin >> mark;
+
+	//if (mark >= 50)
+	//{
+	//	cout << "You passed!" << endl;
+	//}
+	//else
+	//{
+	//	cout << "You failed." << endl;
+	//}
+	readMark(mark);
+	checkPassFail(mark);
 	return 0;
 }
