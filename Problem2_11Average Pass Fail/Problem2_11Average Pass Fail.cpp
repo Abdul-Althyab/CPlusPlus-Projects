f8c5c29c2ee5357a91d91f758ea55b8c9abb6ab2
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-27 10:11:32 +0100

    Refactor main function for modularity and readability
    Refactored the `main` function by breaking it into smaller, reusable
    functions: `readMarks`, `calculateAverage`, and `checkPassFail`.
    Replaced inline logic for reading marks, calculating averages, and
    checking pass/fail status with these new functions. Introduced a
    `marks` array to replace individual mark variables. Improved code
    organization, readability, and reusability.

--- Problem2_11Average Pass Fail/Problem2_11Average Pass Fail.cpp
+++ Problem2_11Average Pass Fail/Problem2_11Average Pass Fail.cpp
@@ -1,19 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	double mark1, mark2, mark3, average;
+void readMarks(double marks[3]) {
 	cout << "Enter your first mark: ";
-	cin >> mark1;
+	cin >> marks[0];
 	cout << "Enter your second mark: ";
-	cin >> mark2;
+	cin >> marks[1];
 	cout << "Enter your third mark: ";
-	cin >> mark3;
-	average = (mark1 + mark2 + mark3) / 3;
-
-	cout <<"Your average is: " << average << endl;
-
+	cin >> marks[2];
+}
+double calculateAverage(double marks[3]) {
+	return (marks[0] + marks[1] + marks[2]) / 3;
+}
+void checkPassFail(const double average) {
+	cout << "Your average is: " << average << endl;
 	if (average >= 50)
 	{
 		cout << "You passed!" << endl;
@@ -22,5 +22,32 @@ int main()
 	{
 		cout << "You failed." << endl;
 	}
+}
+
+int main()
+{
+	double marks[3], average;
+	/*double mark1, mark2, mark3, average;*/
+	//cout << "Enter your first mark: ";
+	//cin >> mark1;
+	//cout << "Enter your second mark: ";
+	//cin >> mark2;
+	//cout << "Enter your third mark: ";
+	//cin >> mark3;
+	//average = (mark1 + mark2 + mark3) / 3;
+
+	//cout <<"Your average is: " << average << endl;
+
+	//if (average >= 50)
+	//{
+	//	cout << "You passed!" << endl;
+	//}
+	//else
+	//{
+	//	cout << "You failed." << endl;
+	//}
+	readMarks(marks);
+	average = calculateAverage(marks);
+	checkPassFail(average);
 	return 0;
 }
