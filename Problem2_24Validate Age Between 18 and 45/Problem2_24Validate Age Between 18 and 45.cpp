baba718e56a12d6112636af50a291d3361b8d652
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-27 10:48:46 +0100

    Refactor age handling for modularity and readability
    Refactored the code to improve modularity by introducing a new
    `readAge(int& age)` function for reading the age and a
    `validateAge(const int age)` function to encapsulate age validation
    logic. Replaced inline validation in `main()` with a call to
    `validateAge()`. Removed outdated commented-out code and updated
    conditional checks to use the new validation function.

--- Problem2_24Validate Age Between 18 and 45/Problem2_24Validate Age Between 18 and 45.cpp
+++ Problem2_24Validate Age Between 18 and 45/Problem2_24Validate Age Between 18 and 45.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
 
+void readAge(int& age)
+{
+	cout << "Enter your age: ";
+	cin >> age;
+}
+
+bool validateAge(const int age)
+{
+	return age >= 18 && age <= 45;
+}
+
 int main()
 {
 	int age;
-	cout << "Enter your age: ";
-	cin >> age;
+	//cout << "Enter your age: ";
+	//cin >> age;
+	readAge(age);
+	validateAge(age);
 
-	if (age >= 18 && age <= 45)
+	if (validateAge(age))
 	{
 		cout << "Your age is valid." << endl;
 	}
