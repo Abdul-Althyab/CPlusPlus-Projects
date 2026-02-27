5a2978ffd4c7d94b9ac9d4f5ec3d9518c8bfe4d7
Abdul-Althyab <abdul.althyab@gmail.com>
2026-02-27 10:54:30 +0100

    Refactor PIN handling into reusable functions
    Encapsulated PIN reading and validation logic into new functions
    `readPIN` and `validatePIN` to improve modularity, readability,
    and maintainability. Replaced inline logic in `main` with calls
    to these functions, separating concerns and enhancing clarity.

--- Problem2_49 ATM PIN/Problem2_49 ATM PIN.cpp
+++ Problem2_49 ATM PIN/Problem2_49 ATM PIN.cpp
@@ -1,14 +1,24 @@
 #include <iostream>
 using namespace std;
 
+void readPIN(int& pin)
+{
+	cout << "Enter your 4-digit PIN: ";
+	cin >> pin;
+}
+bool validatePIN(const int pin)
+{
+	return pin == 1234;
+}
 int main()
 {
 	int pin;
 	double balance = 7500;
-	cout << "Enter your 4-digit PIN: ";
-	cin >> pin;
+	//cout << "Enter your 4-digit PIN: ";
+	//cin >> pin;
+	readPIN(pin);
 
-	if (pin == 1234)
+	if (validatePIN(pin))
 	{
 		cout << "Your Balance is: $"<< balance << endl;
 	}
