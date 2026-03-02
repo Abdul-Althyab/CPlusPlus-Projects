bdfed5d8db38fc3b4cdd89f8dabdf9dd4f0a1af7
Abdul-Althyab <abdul.althyab@gmail.com>
2026-03-02 10:56:14 +0100

    feat: implement month selection using enums and switch case

--- Problem2_45Month of Year/Problem2_45Month of Year.cpp
+++ Problem2_45Month of Year/Problem2_45Month of Year.cpp
@@ -1,13 +1,30 @@
 #include <iostream>
 using namespace std;
 
+enum enMonths 
+{ 
+	January = 1, 
+	February, 
+	March, 
+	April, 
+	May, 
+	June, 
+	July, 
+	August, 
+	September, 
+	October, 
+	November, 
+	December };
+
 int main()
 {
 	int month;
+	enMonths monthOfYear;
 	cout << "Enter a number (1-12) to represent a month of the year: ";
 	cin >> month;
+	monthOfYear = (enMonths)month;
 
-	if (month == 1)
+	/*if (month == 1)
 		cout << "January";
 	else if (month == 2)
 		cout << "February";
@@ -32,5 +49,49 @@ int main()
 	else if (month == 12)
 		cout << "December";
 	else
+		cout << "Invalid input. Please enter a number between 1 and 12.";*/
+	switch (monthOfYear)
+	{
+	case enMonths::January:
+		cout << "January";
+		break;
+	case enMonths::February:
+		cout << "February";
+		break;
+	case enMonths::March:
+		cout << "March";
+		break;
+	case enMonths::April:
+		cout << "April";
+		break;
+	case enMonths::May:
+		cout << "May";
+		break;
+	case enMonths::June:
+		cout << "June";
+		break;
+	case enMonths::July:
+		cout << "July";
+		break;
+	case enMonths::August:
+		cout << "August";
+		break;
+	case enMonths::September:
+		cout << "September";
+		break;
+	case enMonths::October:
+		cout << "October";
+		break;
+	case enMonths::November:
+		cout << "November";
+		break;
+	case enMonths::December:
+		cout << "December";
+		break;
+	default:
 		cout << "Invalid input. Please enter a number between 1 and 12.";
+		break;
+	}
+
+	return 0;
 }
