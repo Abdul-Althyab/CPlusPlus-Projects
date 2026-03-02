da3109652ea067220ee61ef12361d2bf11b42da1
Abdul-Althyab <abdul.althyab@gmail.com>
2026-03-02 10:32:31 +0100

    feat: implement simple calculator using switch statement

--- Problem2_36Simple Calculator/Problem2_36Simple Calculator.cpp
+++ Problem2_36Simple Calculator/Problem2_36Simple Calculator.cpp
@@ -12,20 +12,41 @@ int main()
 	cout << "Enter an operator (+, -, *, /): ";
 	cin >> operation;
 
-	if (operation == '+')
-		cout << number1 << " + " << number2 << " = " << number1 + number2;
-	else if (operation == '-')
-		cout << number1 << " - " << number2 << " = " << number1 - number2;
-	else if (operation == '*')
-		cout << number1 << " * " << number2 << " = " << number1 * number2;
-	else if (operation == '/')
+	//if (operation == '+')
+	//	cout << number1 << " + " << number2 << " = " << number1 + number2;
+	//else if (operation == '-')
+	//	cout << number1 << " - " << number2 << " = " << number1 - number2;
+	//else if (operation == '*')
+	//	cout << number1 << " * " << number2 << " = " << number1 * number2;
+	//else if (operation == '/')
+	//{
+	//	if (number2 != 0)
+	//		cout << number1 << " / " << number2 << " = " << (double)number1 / number2;
+	//	else
+	//		cout << "Error: Division by zero is not allowed.";
+	//}
+	//else
+	//	cout << "Error: Invalid operator.";
+	switch (operation)
 	{
+	case '+':
+		cout << number1 << " + " << number2 << " = " << number1 + number2;
+		break;
+	case '-':
+		cout << number1 << " - " << number2 << " = " << number1 - number2;
+		break;
+	case '*':
+		cout << number1 << " * " << number2 << " = " << number1 * number2;
+		break;
+	case '/':
 		if (number2 != 0)
 			cout << number1 << " / " << number2 << " = " << (double)number1 / number2;
 		else
 			cout << "Error: Division by zero is not allowed.";
-	}
-	else
+		break;
+	default:
 		cout << "Error: Invalid operator.";
-
+		break;
+	}
+	return 0;
 }
