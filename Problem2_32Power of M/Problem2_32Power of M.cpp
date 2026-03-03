4374ad78ba703b8c14b8e87019f82b7233b4678b
Abdul-Althyab <abdul.althyab@gmail.com>
2026-03-03 11:55:44 +0100

    feat: add modular power calculation using a for loop

--- Problem2_32Power of M/Problem2_32Power of M.cpp
+++ Problem2_32Power of M/Problem2_32Power of M.cpp
@@ -2,22 +2,49 @@
 #include <cmath>
 using namespace std;
 
-double power(double base, double exponent)
+double ReadBase()
 {
-	return pow(base, exponent);
+	double base;
+	cout << "Please enter the base: ";
+	cin >> base;
+	cout << endl;
+	return base;
+}
+int ReadExponent()
+{
+	int exponent;
+	cout << "Please enter the exponent: ";
+	cin >> exponent;
+	cout << endl;
+	return exponent;
+}
+
+double power(double base, int exponent)
+{
+	if (base == 0 && exponent < 0)
+	{
+		cout << "Error: 0 raised to a non-positive exponent is undefined." << endl;
+		return NAN; // Return Not-A-Number to indicate an error
+	}
+	double result = 1;
+	int absExponent = abs(exponent);
+	for (int i = 1; i <= absExponent; ++i)
+	{
+		result *= base;
+	}
+	if (exponent < 0)
+	{
+		result = 1 / result;
+	}
+	return result;
 }
 
 int main()
 {
-	double base, exponent, result;
-	cout << "This program calculates the power of M\n" << endl;
-	cout << "Please enter the base: ";
-	cin >> base;
-	cout << endl;
-	cout << "Please enter the exponent: ";
-	cin >> exponent;
-	cout << endl;
-	result = power(base, exponent);
-	cout << base << " raised to the power of " << exponent << " is: " << result;
+	double base;
+	int exponent;
+	base = ReadBase();
+	exponent = ReadExponent();
+	cout << base << " raised to the power of " << exponent << " is: " << power(base, exponent) << endl;
 	return 0;
 }
